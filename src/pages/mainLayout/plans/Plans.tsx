4aa6668be2d8dcabd4c7ae711b78c15cb919ab1f
Luis Fernando Cerqueira <fernando.luis953@gmail.com>
2023-10-23 14:57:18 -0300

    adding test payment page

---
+++ src/pages/mainLayout/plans/Plans.tsx
@@ -0,0 +1,9 @@
+import { useEffect } from 'react';
+
+export default function Plans() {
+  useEffect(() => {
+    window.open('https://buy.stripe.com/test_fZeg0qajF0S079S3cc');
+  }, []);
+
+  return <div></div>;
+}
